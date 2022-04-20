94c62f7002cf3d68ffb03dc5de0ede9604f403c9
petu <petteri.helttula@metropolia.fi>
2022-04-20 13:40:47 +0300

    moro

--- app/src/main/java/com/example/kaloripaivakirja/MainActivity.java
+++ app/src/main/java/com/example/kaloripaivakirja/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
      * Ryhmä 1 sovellus kaloripäiväkirja
      *
      */
+    // moromoro
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
