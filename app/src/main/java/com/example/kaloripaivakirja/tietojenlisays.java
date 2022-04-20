823c964969e92462b548984435505922cfc6d51b
Jere <hippelainen.jere@gmail.com>
2022-04-20 14:36:51 +0300

    lisätty tietojenlisäys aktiviteetti

---
+++ app/src/main/java/com/example/kaloripaivakirja/tietojenlisays.java
@@ -0,0 +1,14 @@
+package com.example.kaloripaivakirja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tietojenlisays extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tietojenlisays);
+    }
+}
