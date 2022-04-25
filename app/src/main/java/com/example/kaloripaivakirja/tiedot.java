99b2bc648997bf30560d504b12510711df9e3131
Jere <hippelainen.jere@gmail.com>
2022-04-25 12:40:29 +0300

    omien tietojen muokkausnäkymään pääsee napeilla

--- app/src/main/java/com/example/kaloripaivakirja/tiedot.java
+++ app/src/main/java/com/example/kaloripaivakirja/tiedot.java
@@ -2,7 +2,10 @@ package com.example.kaloripaivakirja;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class tiedot extends AppCompatActivity {
 
@@ -10,6 +13,17 @@ public class tiedot extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tiedot);
+        Button muokkaa = findViewById(R.id.muokkaaButton);
+        muokkaa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                muokkaaTietoja();
+            }
+        });
+    }
+    public void muokkaaTietoja(){
+        Intent muokkaaTiedot = new Intent(this, tietojenlisays.class);
+        startActivity(muokkaaTiedot);
     }
     //henkilö class josta otetaan tiedot tähän näkymään
 }
